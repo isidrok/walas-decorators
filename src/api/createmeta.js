1d4d6e0ddbcbef68bb7aedbe720d747a803d8bca
Isidro Torregrosa Torralba <itorregrosa@alfatecsistemas.es>
2017-04-03 09:22:53 +0200

    project setup

--- src/api/createmeta.js
+++
@@ -1,6 +0,0 @@
-import { META } from './meta';
-export const createMeta = function (target) {
-    if (!target) throw new Error('Target does not exist');
-    if (target.name) return target[META] = (target[META] || {});
-    return target.constructor[META] = (target.constructor[META] || {});
-}
--- src/api/meta.js
+++
@@ -1,2 +0,0 @@
-export const META = 'META';
-// export const META = Symbol('META');
