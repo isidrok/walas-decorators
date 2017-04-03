6ca04cf123e6cbf8c54efa28fafcccb87d0ddc41
Isidro Torregrosa Torralba <itorregrosa@alfatecsistemas.es>
2017-04-03 14:02:39 +0200

    fix description

--- src/decorators/rest/description.js
+++ src/decorators/rest/description.js
@@ -1,12 +1,12 @@
 // Specifies the description of
-
-
-
-let source = getSource(descriptor);
-let key  = 'description';
-if(source!=='class'){
-   let container = (source === 'method' ? 'methods' : 'properties');
-   key = `${container}.${key}.description`
-}
-setMeta(target,key,params[0]);
-
+// a class, method or property.
+import { setMeta } from 'walas-meta-api';
+import { getSource } from '../utils';
+export const description = function (...params) {
+    return function (target, key, descriptor) {
+        let source = getSource(descriptor);
+        let container = (source === 'method' ? 'methods' : 'properties');
+        key = source === 'class' ? 'description' : `${container}.${key}.description`;
+        setMeta(target, key, params[0]);
+    }
+};
