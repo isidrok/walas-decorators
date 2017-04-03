00184574a6a345b15cbd2eb243064381fa8672c5
Pedro Hurtado <pedrohurt@gmail.com>
2017-04-03 13:41:10 +0200

    Update ignore.js

--- src/decorators/model/ignore.js
+++ src/decorators/model/ignore.js
@@ -1,7 +1,7 @@
 import { setMeta } from 'walas-meta-api';
-import { getSource, validateArgs } from '../utils';
+import { getSource} from '../utils';
 
-export const ignore = function (...params) {
+export const ignore = function () {
     return function (target, key, descriptor) {
         let source = getSource(descriptor);
         setMeta(target, `properties.${key}.ignore`, true);
