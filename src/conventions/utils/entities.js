ce9939cad5dfa0ae3327eb9609c7bf28b29d2169
Isidro Torregrosa Torralba <itorregrosa@alfatecsistemas.es>
2017-04-04 13:51:59 +0200

    conventions structure

--- src/conventions/utils/entities.js
+++ src/conventions/utils/entities.js
@@ -1,6 +1,15 @@
-import {META} from '../../api'
+import { META } from '../../api'
 export const getEntities = function (dbContext, instance) {
-    return Object.entries(Object.getOwnPropertyDescriptors(dbContext.prototype))
+    let entities = Object.entries(Object.getOwnPropertyDescriptors(dbContext.prototype))
         .filter(c => c[0] !== 'constructor' && typeof c[1].value !== 'function')
         .map(c => ({ id: c[0], meta: instance[c[0]]._entity.constructor[META] || {} }));
+    return parseEntities(entities);
+}
+
+function parseEntities(entities) {
+    let obj = {};
+    entities.forEach(entity => {
+        obj[entity.id] = entity.meta;
+    });
+    return obj;
 }
